0cc8794c38d8f484b1a6b2ba7a57e3b816eda327
ChungEun Jennifer Chae <cchae216@gmail.com>
2023-11-10 10:36:12 -0500

    fix stripe

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,10 @@
 //dev.js - don't commit this.
 
 module.exports = {
-    googleClientID: process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret:  process.env.GOOGLE_CLIENT_SECRET,
-    mongoURI: process.env.MONGO_URI,
-    cookieKey: process.env.COOKIE_KEY, // could be anything!
-}
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY, // could be anything!
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+};
--- index.js
+++ index.js
@@ -1,33 +1,32 @@
-const express = require("express");
+const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
 const passport = require('passport');
 // require statement just makes sure "passport.js" code gets executed.
 // No need to assign it in a variable.
-const cookieSession = require("cookie-session");
+const cookieSession = require('cookie-session');
 
 require('./models/User'); // Import User model class!
 require('./services/passport');
 
-mongoose.connect(keys.mongoURI)
-
+mongoose.connect(keys.mongoURI);
+console.log(keys.mongoURI);
 const app = express();
 app.use(
-    cookieSession({
-        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 DAYS in milliseconds before cookie expires.
-        keys: [keys.cookieKey],
-    })
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 DAYS in milliseconds before cookie expires.
+    keys: [keys.cookieKey],
+  })
 );
 app.use(passport.initialize());
 app.use(passport.session());
 
 // when we require authRoutes file, it returns a "FUNCTION".
 // then immediatley call that function with 'app' variable.
-require('./routes/authRoutes')(app); 
+require('./routes/authRoutes')(app);
 
-
-app.get("/", (req,res) => {
-    res.send({hi: "there"})
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
 });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
