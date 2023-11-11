56112a748fae4d10ddddfd5d3909008d50ca7281
ChungEun Jennifer Chae <cchae216@gmail.com>
2023-11-11 17:46:28 -0500

    untrack dev.js

---
+++ config/dev.js
@@ -0,0 +1,14 @@
+//dev.js - don't commit this.
+
+module.exports = {
+  googleClientID:
+    '202524647268-vdfqhvc4np18ck0kflv3q2u81grubph1.apps.googleusercontent.com',
+  googleClientSecret: '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',
+  mongoURI:
+    'mongodb+srv://cchae216:UKAjchNs62XLuN7M@cluster0.pcduvry.mongodb.net/?retryWrites=true&w=majority',
+  cookieKey: 'akidfhusaidfuhewlafdgjhdjhsgsxf', // could be anything!
+  stripePublishableKey:
+    'pk_test_51O5ykJBydZobwKJNQip5Ct2HHWd5PFUGChOGOzwUqx31cuWZvppmWtyXZigEtBWeqWhYO5coCIDaOMDBfLw6Nk4A00QPrE8s8z',
+  stripeSecretKey:
+    'sk_test_51O5ykJBydZobwKJNqQP0tCZCUNekz0fHOZegtx9PQ1jwWPimbdTu1bTo2BUzQnuFrgOEJbfY8r8nBBXQ2fDvkGtl001R2qpH41',
+};
